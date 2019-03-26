9b6d4f37eace3781e351fe9676b9fed4c8527513
Braden <spacejunque@outlook.com>
2019-03-25 20:00:00 -0400

    added rank to users, rank-protected buttons and reports page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :texts
   validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@shadysideacademy\.org\z/, message: "must be a Shady Side Academy email" }
 end
